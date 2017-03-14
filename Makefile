0596fd3e7876eabdf03672d7f30953000eadca2b
Jake Brisk <jakebrisk@gmail.com>
2017-03-14 19:08:14 -0400

    recursive approach

---
+++ Makefile
@@ -0,0 +1,30 @@
+
+FLAGS = -Wall -std=c++11
+
+all:	p1 p2 p3
+
+p1:	program1.o
+	g++ $(FLAGS) program1.o -o program1
+
+program1.o:	program1.cpp
+	g++ -c $(FLAGS) program1.cpp -o program1.o
+
+p2:	program2.o	
+	g++ $(FLAGS) program2.o -o program2
+
+program2.o:	program2.cpp
+	g++ -c $(FLAGS) program2.cpp -o program2.o
+
+p3:	program3.o	
+	g++ $(FLAGS) program3.o -o program3
+
+program3.o:	program3.cpp
+	g++ -c $(FLAGS) program3.cpp -o program3.o
+
+clean:
+	rm -f *.o program1 program2 program3
+
+run: p1 p2 p3
+	./program1 ${args} output1.txt
+	./program2 ${args} output2.txt
+	./program3 ${args} output3.txt
---
+++ program2.cpp
@@ -0,0 +1,91 @@
+/** 
+ *	This program caculates the LCS of two strings using recursive approach
+ *
+ *	@author Jake Brisk
+ *	@version 1.0 3/14/17
+ */
+
+#include <string>		// for strings and such
+#include <iostream>		// cin, cout
+#include <fstream>		// file I/O
+#include <vector>		// for lookup table
+#include <chrono>		// to measure running time
+#include <algorithm>
+
+
+using namespace std;
+
+int lcs_length(string x, string y);
+
+
+int main(int argc, char const *argv[]) {
+	
+	// Expects argc to be 4 
+	if(argc < 4){
+		cout << "Program usage is: program1 <filex.txt> <filey.txt> <output1.txt>" << endl;
+		exit(1);
+	}
+
+	ifstream xfile, yfile, ofile; // input and output files
+	string xline, yline; // input strings
+
+	xfile.open(argv[1]); // assumes argv[1] is file containing x
+	yfile.open(argv[2]); // assumes argv[2] is file containing y
+
+	// try to read from files, exit if they're empty
+	if(! (getline(xfile, xline) && getline(yfile, yline))){ 
+		cerr << "Couldn't read line from file" << endl;
+		exit(1);
+	}
+
+	xfile.close();
+	yfile.close();
+
+	// start timing the program execution
+	auto start = chrono::high_resolution_clock::now();
+
+	// get the lcs length
+	int length = lcs_length(xline, yline);
+
+	cout << length << endl;
+
+
+	// end the timer
+	auto end = chrono::high_resolution_clock::now();
+	// calculate the duration
+	auto span = chrono::duration_cast<chrono::duration<double>>(end - start);
+
+	/**********        OUTPUT SECTION          *************/
+
+	// open the output file and write to it
+	ofile.open(argv[3]); // assumes argv[3] is the output file
+
+	// out put lcs length
+	ofile << length << endl;
+	// output the running time
+	ofile << span.count() << endl;
+	ofile.close();
+
+	return 0;
+}
+
+
+int lcs_length(string x, string y){
+
+	// base case x or y is ""
+	if(x.length() < 1 || y.length() < 1)
+		return 0;
+
+	// strone lengths of both strings
+	int xlength = x.length(), ylength = y.length();
+
+	if(x[xlength-1] == y[ylength-1])
+		// last character of x = last character of y. Return 1 + result for 
+		// x[0...len-1] and y[0..len-1]
+		return 1 + lcs_length(x.substr(0, xlength-1), y.substr(0, ylength-1));
+	else 
+		// last characters are not the same.
+		// return max of x[0...len-1], y[0...len] and x[0...len], y[0...len-1]
+		return max(lcs_length(x.substr(0, xlength-1), y.substr(0, y.length())), 
+							lcs_length(x.substr(0, x.length()), y.substr(0, y.length()-1)));
+}
